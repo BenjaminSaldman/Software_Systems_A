bcfd2b11c7475642cef6c93b57932d79b7c386dd
TomKoltunov <74180693+TomKoltunov@users.noreply.github.com>
2021-11-06 22:47:24 +0200

    Version_16

--- main.c
+++ main.c
@@ -1,47 +1,46 @@
-#include <stdio.h>
 #include "NumClass.h"
-#include <math.h>
+#include <stdio.h>
 
-int main(){
-    int num1;
-    int num2;
-    int swap;
-    scanf("%d %d",&num1,&num2);
-    if(num2>num1){
-        swap=num1;
-        num1=num2;
-        num2=swap;
+int main()
+{
+    int a, b;
+    scanf("%d %d",&a,&b);
+    if (a > b)
+    {
+        int swap = b;
+        b = a;
+        a = swap;
     }
-    int i;
-    int j;
-    for (i=1;i<=4;i++){
-        if(i==1){
-            printf("The Armstrong numbers are:");
+    printf("The Armstrong numbers are:");
+    for(int i = a; i <= b; i++)
+    {
+        if(isArmStrong(i)==true){
+            printf(" %d", i);
         }
-        if(i==2){
-            printf("The Palindromes are:");
-        }
-        if(i==3){
-            printf("The Prime numbers are:");
-        }
-        if(i==4){
-            printf("The Strong numbers are:");
-        }
-        for(j=num2;j<=num1;j++){
-            if(i==1 && isArmStrong(j)==1){
-                printf(" %d",j);
-            }
-            if(i==2 && isPalindrome(j)==1){
-                printf(" %d",j);
-            }
-            if(i==3 && isPrime(j)==1){
-                printf(" %d",j);
-            }
-            if(i==4 && isStrong(j)==1){
-                printf(" %d",j);
-            }
-        
-        }
-        printf("\n");
     }
+    printf("\n");
+    printf("The Palindromes are:");
+    for(int i = a; i <= b; i++)
+    {
+        if(isPalindrome(i)==true){
+            printf(" %d", i);   
+        }
+    }
+    printf("\n");
+    printf("The Prime numbers are:");
+    for(int i = a; i <= b; i++)
+    {
+        if(isPrime(i)==true){
+            printf(" %d", i);       
+        }
+    }
+    printf("\n");
+    printf("The Strong numbers are:");
+    for(int i = a; i <= b; i++)
+    {
+        if(isStrong(i)==true){
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
 }
